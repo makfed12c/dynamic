b5628b52f573c353e85874489df56106d2c0a736
makfed12c <zhbxueer@126.com>
2024-12-26 09:40:19 +0800

    fixed configuration fetching

--- src/grindurus-frontend/src/components/pool/panel/configuration/Configuration.tsx
+++ src/grindurus-frontend/src/components/pool/panel/configuration/Configuration.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { Strategy__factory, Strategy } from '../../../../typechain-types'
 import { FormGroup, Select, Option } from '../../../ui'
 import { useProtocolContext } from '../../../../context/ProtocolContext'
+import { IPoolsNFTLens } from '../../../../typechain-types/PoolsNFT'
 
 type ConfigurationProps = {
   poolId: number
@@ -13,22 +14,45 @@ const Configuration = ({ poolId }: ConfigurationProps) => {
 
   const [pool, setPool] = useState<Strategy | null>(null)
 
-  const [longNumberMax, setLongNumberMax] = useState<number>(4)
-  const [hedgeNumberMax, setHedgeNumberMax] = useState<number>(4)
-  const [extraCoef, setExtraCoef] = useState<number>(0)
-  const [priceVolatilityPercent, setPriceVolatilityPercent] = useState<number>(1)
+  const [longNumberMax, setLongNumberMax] = useState<number>(3)
+  const [inputLongNumberMax, setInputLongNumberMax] = useState<number>(3)
+  const [hedgeNumberMax, setHedgeNumberMax] = useState<number>(3)
+  const [inputHedgeNumberMax, setInputHedgeNumberMax] = useState<number>(3)
+  const [extraCoef, setExtraCoef] = useState<string>("2.00")
+  const [inputExtraCoef, setInputExtraCoef] = useState<string>("2.00")
+  const [priceVolatilityPercent, setPriceVolatilityPercent] = useState<string>("1.00")
+  const [inputPriceVolatilityPercent, setInputPriceVolatilityPercent] = useState<string>("1.00")
   const [selectedOpReturnPercent, setSelectedOpReturnPercent] = useState<number>(1)
   const [inputReturnPercent, setInputReturnPercent] = useState<number>(0)
+  const [longSellReturnPercent, setLongSellReturnPercent] = useState<string>("100.5")
+  const [hedgeSellReturnPercent, setHedgeSellReturnPercent] = useState<string>("100.5")
+  const [hedgeRebuyReturnPercent, setHedgeRebuyReturnPercent] = useState<string>("100.5")
   const [selectedOpFeeCoef, setSelectedOpFeeCoef] = useState<number>(1)
   const [inputFeeCoef, setInputFeeCoef] = useState<number>(0)
+  const [longSellFeeCoef, setLongSellFeeCoef] = useState<string>("100.5")
+  const [hedgeSellFeeCoef, setHedgeSellFeeCoef] = useState<string>("100.5")
+  const [hedgeRebuyFeeCoef, setHedgeRebuyFeeCoef] = useState<string>("100.5")
 
   useEffect(() => {
     initPool()
-  }, [signer])
+  }, [signer, poolsNFT])
 
   const initPool = async () => {
     const poolAddress = await poolsNFT!.pools(poolId)
     const contract = Strategy__factory.connect(poolAddress, signer)
+    const poolsNFTInfos: IPoolsNFTLens.PoolNFTInfoStructOutput[] = await poolsNFT!.getPoolNFTInfosBy([poolId])
+    const config = poolsNFTInfos[0].config
+    setLongNumberMax(Number(config.longNumberMax))
+    setHedgeNumberMax(Number(config.hedgeNumberMax))
+    setExtraCoef((Number(config.extraCoef) / 100).toFixed(2))
+    setPriceVolatilityPercent((Number(config.priceVolatilityPercent) / 100).toFixed(2))
+    setLongSellReturnPercent((Number(config.returnPercentLongSell) / 100).toFixed(2))
+    setHedgeSellReturnPercent((Number(config.returnPercentHedgeSell) / 100).toFixed(2))
+    setHedgeRebuyReturnPercent((Number(config.returnPercentHedgeRebuy) / 100).toFixed(2))
+    const feeConfig = poolsNFTInfos[0].feeConfig
+    setLongSellFeeCoef(((Number(feeConfig.longSellFeeCoef)) / 100).toFixed(2))
+    setHedgeSellFeeCoef(((Number(feeConfig.hedgeSellFeeCoef)) / 100).toFixed(2))
+    setHedgeRebuyFeeCoef(((Number(feeConfig.hedgeRebuyFeeCoef)) / 100).toFixed(2))
     setPool(contract)
   }
 
@@ -41,7 +65,7 @@ const Configuration = ({ poolId }: ConfigurationProps) => {
   const handleSetLongNumberMax = async () => {
     checkRequired()
     try{
-      const tx = await pool!.setLongNumberMax(longNumberMax)
+      const tx = await pool!.setLongNumberMax(inputLongNumberMax)
       await tx.wait()
     } catch(err) {
       console.log("Failed to set long number max", err)
@@ -51,7 +75,7 @@ const Configuration = ({ poolId }: ConfigurationProps) => {
   const handleSetHedgeNumberMax = async () => {
     checkRequired()
     try{
-      const tx = await pool!.setHedgeNumberMax(hedgeNumberMax)
+      const tx = await pool!.setHedgeNumberMax(inputHedgeNumberMax)
       await tx.wait()
     } catch(err) {
       console.log("Failed to set hedge number max", err)
@@ -61,7 +85,7 @@ const Configuration = ({ poolId }: ConfigurationProps) => {
   const handleSetExtraCoef = async () => {
     checkRequired()
     try{
-      const extraCoefRaw = Math.round(extraCoef * 100)
+      const extraCoefRaw = Math.round(parseFloat(inputExtraCoef) * 100)
       const tx = await pool!.setExtraCoef(extraCoefRaw)
       await tx.wait()
     } catch(err) {
@@ -72,7 +96,7 @@ const Configuration = ({ poolId }: ConfigurationProps) => {
   const handleSetPriceVolatilityPercent = async () => {
     checkRequired()
     try{
-      const priceVolatilityPercentRaw = Math.round(priceVolatilityPercent * 100)
+      const priceVolatilityPercentRaw = Math.round(parseFloat(inputPriceVolatilityPercent) * 100)
       const tx = await pool!.setPriceVolatilityPercent(priceVolatilityPercentRaw)
       await tx.wait()
     } catch(err) {
@@ -116,7 +140,7 @@ const Configuration = ({ poolId }: ConfigurationProps) => {
               <input
                 className="input-field"
                 placeholder='Long number max example: 2'
-                onChange={(e) => setLongNumberMax(Number(e.target.value))}
+                onChange={(e) => setInputLongNumberMax(Number(e.target.value))}
               />
               <button
                 className={`${styles["button"]} button`}
@@ -131,7 +155,7 @@ const Configuration = ({ poolId }: ConfigurationProps) => {
               <input
                 className="input-field"
                 placeholder='Hedge number max example: 4'
-                onChange={(e) => setHedgeNumberMax(Number(e.target.value))}
+                onChange={(e) => setInputHedgeNumberMax(Number(e.target.value))}
               />
               <button 
                 className={`${styles["button"]} button`}
@@ -147,7 +171,7 @@ const Configuration = ({ poolId }: ConfigurationProps) => {
             <div className="form-input">
               <input
                 placeholder='Extra coef example: 2.00'
-                onChange={(e) => setExtraCoef(parseFloat(e.target.value))}
+                onChange={(e) => setInputExtraCoef(e.target.value)}
               />
               <button
                 className={`${styles["button"]} button`}
@@ -162,7 +186,7 @@ const Configuration = ({ poolId }: ConfigurationProps) => {
               <input
                 className="input-field"
                 placeholder='Price volatility percent example: 1.4%'
-                onChange={(e) => setPriceVolatilityPercent(parseFloat(e.target.value))}
+                onChange={(e) => setInputPriceVolatilityPercent(e.target.value)}
               />
               <button 
                 className={`${styles["button"]} button`}
@@ -174,34 +198,42 @@ const Configuration = ({ poolId }: ConfigurationProps) => {
           </FormGroup>
         </div>
         <div className={styles["row"]}>
-          <FormGroup label="Operation & Return Percent" className={styles["form-group"]}>
+          <FormGroup label={`Operation & Return Percent (Current value: ${
+            selectedOpReturnPercent === 1 ? longSellReturnPercent :
+            selectedOpReturnPercent === 2 ? hedgeSellReturnPercent :
+            selectedOpReturnPercent === 3 ? hedgeRebuyReturnPercent : ""
+            }%)`} className={styles["form-group"]}>
             <div className={styles["operation-row"]}>
               <Select onChange={(value) => setSelectedOpReturnPercent(value as number)} className={styles["select"]}>
-                <Option value={1}>
-                  Long sell
-                </Option>
-                <Option value={2}>
-                  Hedge sell
-                </Option>
-                <Option value={3}>
-                  Hedge buy
-                </Option>
+              <Option value={1}>
+                Long sell
+              </Option>
+              <Option value={2}>
+                Hedge sell
+              </Option>
+              <Option value={3}>
+                Hedge buy
+              </Option>
               </Select>
               <div className={`${styles["form-input"]} form-input`}>
-                <input
-                  placeholder='Return percent example: 1.0'
-                  onChange={(e) => setInputReturnPercent(parseFloat(e.target.value))}
-                />
-                <button
-                  className={`${styles["button"]} button`}
-                  onClick={() => handleSetReturnPercent()}  
-                >
-                  Set
-                </button>
+              <input
+                placeholder='Return percent example: 1.0'
+                onChange={(e) => setInputReturnPercent(parseFloat(e.target.value))}
+              />
+              <button
+                className={`${styles["button"]} button`}
+                onClick={() => handleSetReturnPercent()}  
+              >
+                Set
+              </button>
               </div>
             </div>
-          </FormGroup>
-          <FormGroup label="Operation & Fee Coefficient" className={styles["form-group"]}>
+            </FormGroup>
+          <FormGroup label={`Operation & Fee Coef (Current value: x${
+            selectedOpFeeCoef === 1 ? longSellFeeCoef :
+            selectedOpFeeCoef === 2 ? hedgeSellFeeCoef :
+            selectedOpFeeCoef === 3 ? hedgeRebuyFeeCoef : ""
+            })`} className={styles["form-group"]}>
             <div className={styles["operation-row"]}>
               <Select onChange={(value) => setSelectedOpFeeCoef(value as number)} className={styles["select"]}>
                 <Option value={1}>
