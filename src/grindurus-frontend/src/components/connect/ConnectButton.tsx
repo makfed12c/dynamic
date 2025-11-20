65e2058ab3bde64087938f19601f8ca0054c9393
makfed12c <zhbxueer@126.com>
2025-11-21 04:52:20 +0800

    added mobile adaptivity to header

--- src/grindurus-frontend/src/components/connect/ConnectButton.tsx
+++ src/grindurus-frontend/src/components/connect/ConnectButton.tsx
@@ -2,13 +2,17 @@ import { useAppKitAccount, useAppKit } from '@reown/appkit/react'
 import { shortenAddress } from '../../utils/addresses'
 import styles from './ConnectButton.module.scss'
 
-export default function ConnectButton() {
+interface ConnectButtonProps {
+  className?: string
+}
+
+export default function ConnectButton({ className }: ConnectButtonProps) {
   const { open } = useAppKit()
   const { address, isConnected } = useAppKitAccount()
 
   return (
     <button 
-      className={`${styles["button"]} button`}
+      className={`${styles["button"]} button ${className ?? ''}`}
       onClick={() => open()}
     >
       {isConnected && address ? shortenAddress(address) : "Connect Wallet"}
--- src/grindurus-frontend/src/components/header/Header.tsx
+++ src/grindurus-frontend/src/components/header/Header.tsx
@@ -1,31 +1,43 @@
 import styles from './Header.module.scss'
-import React from 'react'
+import { useState } from 'react'
 import { useAppKit, useAppKitNetwork } from '@reown/appkit/react'
 import { Link } from 'react-router-dom'
 import ConnectButton from '../connect/ConnectButton'
 import logo from '../../assets/logo.svg'
+import menuIcon from '../../assets/icons/menu.svg'
+import closeIcon from '../../assets/icons/close.svg'
 
 function Header() {
   const { open } = useAppKit()
   const { caipNetwork } = useAppKitNetwork()
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
 
   return (
-    <header className={styles.header}>
+    <header className={styles["header"]}>
       <div className={`${styles["container"]} container`}>
-        <Link to="/" className={styles['logo']}>
+        <Link to="/" className={styles["logo"]}>
           <img src={logo} alt="GrindURUS Logo" />
-          <div>GrindURUS</div>
+          <div className={styles["text"]}>GrindURUS</div>
         </Link>
-        <nav className={styles['navigation']}>
-          <ul className={`${styles['menu']} ${styles.menu}`}>
+        <nav className={`${styles['navigation']} ${isMenuOpen ? styles["active"] : ""}`}>
+          <ul className={styles['menu']}>
             <li className={styles['menu-item']}><Link to="/" className={styles['menu-link']}>Dashboard</Link></li>
             <li className={styles['menu-item']}><Link to="/greth" className={styles['menu-link']}>grETH</Link></li>
             {/* <li className={styles['menu-item']}><Link to="grinder-ai" className={styles['menu-link']}>GrinderAI</Link></li> */}
             <li className={styles['menu-item']}><Link to="grai" className={styles['menu-link']}>grAI</Link></li>
           </ul>
+          <div className={styles["navigation-buttons"]}>
+            <button className={`${styles["button"]} ${styles['network-button']} button`} onClick={() => open({view: 'Networks'})}>
+              <div className={styles['button-image']}>
+                <img src={caipNetwork?.assets?.imageUrl} alt="Chain Icon" />
+              </div>
+              {caipNetwork?.name}
+            </button>
+            <ConnectButton className={styles["connect-button"]} />
+          </div>
         </nav>
         <div className={styles['buttons']}>
-          <button className={`${styles.button} ${styles['network-button']} button`} onClick={() => open({view: 'Networks'})}>
+          <button className={`${styles["button"]} ${styles['network-button']} button`} onClick={() => open({view: 'Networks'})}>
             <div className={styles['button-image']}>
               <img src={caipNetwork?.assets?.imageUrl} alt="Chain Icon" />
             </div>
@@ -33,6 +45,14 @@ function Header() {
           </button>
           <ConnectButton />
         </div>
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)} className={`${styles["menu-button"]} ${isMenuOpen ? styles["active"] : ""}`}>
+          <div className={styles["open"]}>
+            <img src={menuIcon} alt="Menu Icon" />
+          </div>
+          <div className={styles["close"]}>
+            <img src={closeIcon} alt="Close Icon" />
+          </div>
+        </button>
       </div>
     </header>
   )
